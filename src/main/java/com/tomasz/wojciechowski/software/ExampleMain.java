339b5c2458212b24420b7d65973bbe44ba6bad75
Tomasz Wojciechowski <tomizna.26@gmail.com>
2020-03-01 18:10:15 +0100

    personalized message for every user

--- src/main/java/com/tomasz/wojciechowski/software/ExampleMain.java
+++ src/main/java/com/tomasz/wojciechowski/software/ExampleMain.java
@@ -1,8 +1,13 @@
 package com.tomasz.wojciechowski.software;
 
+import java.util.Optional;
+
 public class ExampleMain {
     public static void main(String[] args){
-        System.out.println("Hello World");
+        String message = Optional.ofNullable( args.length >0 ? args[0] : null)
+                .map( name -> "Hello "+name)
+                .orElse("Hello World");
+        System.out.println(message);
     }
 }
 
